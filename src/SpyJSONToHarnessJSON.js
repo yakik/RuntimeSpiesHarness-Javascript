2066c9eb74d9de9bd1a3f313dce202b9785727e4
Yaki <yaki.koren@gmail.com>
2018-10-20 22:53:44 +0300

    Function JSON

--- src/SpyJSONToHarnessJSON.js
+++ src/SpyJSONToHarnessJSON.js
@@ -4,6 +4,9 @@ var spyJSONToHarnessJSON = function(spyJSON) {
     spyJSON.variables.forEach(variable => {
         harnessJSON.push(getVariableHarness(variable))
     });
+    spyJSON.functions.forEach(functionSpy => {
+        harnessJSON.push(getFunctionHarness(functionSpy))
+    });
     return harnessJSON
 }
 
@@ -18,5 +21,33 @@ var getVariableHarness = function(variable){
     return returnedJSON
 }
 
+var getFunctionHarness = function(functionSpyJSON){
+    var returnedJSON = {
+        functionHarness: [
+            { variableDefinition: getFunctionDBDefinition(functionSpyJSON) },
+            { variableDefinition: { name: functionSpyJSON.name + '_counter', value: 0 } },
+            { functionDefinition: getFunctionDefinition(functionSpyJSON) }]
+    }
+    return returnedJSON
+}
+
+var getFunctionDefinition = function (functionSpyJSON) {
+    var returnedJSON = {
+            name: functionSpyJSON.name,
+            content: [
+                { validateInputAndGetOutput: { function: functionSpyJSON.name, DB: functionSpyJSON.name + '_DB', counter: functionSpyJSON.name + '_counter', toVariable: 'output' } },
+                { increaseCounterByOne: { counter: functionSpyJSON.name + '_counter' } },
+                { returnOutput: { returnedVariable: 'output' } }]
+        }
+        return returnedJSON
+}
+
+var getFunctionDBDefinition=function(functionSpyJSON){
+    var returnedJSON = { name: functionSpyJSON.name + '_DB', value:[] }
+    functionSpyJSON.traffic.forEach(call => {
+        returnedJSON.value.push({ arguments: call.arguments, returnValue: call.returnValue })
+    })
+    return returnedJSON
+}
 
 module.exports = spyJSONToHarnessJSON
---
+++ test/SpyJSONToHarnessJSONTests.js
@@ -0,0 +1,77 @@
+var mocha = require('mocha')
+var chai = require('chai')
+var expect = chai.expect
+var spyJSONToHarnessJSON = require('../src/SpyJSONToHarnessJSON')
+
+mocha.describe('Get Code JSON From Spy JSON', function () {
+
+    mocha.it('One Variable', function () {
+
+        var spyJSON = {
+            testedFunctionCall: 'testFunction(a)',
+            resultLiteral: 'NOTSET',
+            variables:
+                [{
+                    name: 'a',
+                    values: [{
+                        timing: 'Initial',
+                        value: 2
+                    }]
+                }
+                ],
+            functions: []
+        }
+        var expectedJSON = [
+            { variableDefinition: { name: 'a', value: 2 } }
+        ]
+        expect(spyJSONToHarnessJSON(spyJSON)).to.deep.equal(expectedJSON)
+    })
+    mocha.it('One Function', function () {
+
+        var spyJSON = {
+            testedFunctionCall: 'testFunction(a)',
+            resultLiteral: 'NOTSET',
+            variables: [],
+            functions: [{
+                name: 'a',
+                traffic: [{
+                    callNumber: 0,
+                    arguments: [1, 5],
+                    returnValue: 6
+                }, {
+                    callNumber: 1,
+                    arguments: [5, -3],
+                    returnValue: 2
+                }]
+            }]
+        }
+        var expectedJSON = [
+            {
+                functionHarness: [
+                    {
+                        variableDefinition: {
+                            name: 'a_DB',
+                            value: [{
+                                arguments: [1, 5],
+                                returnValue: 6
+                            }, {
+                                arguments: [5, -3],
+                                returnValue: 2
+                            }]
+                        }
+                    }, { variableDefinition: { name: 'a_counter', value: 0 } },
+                    {
+                        functionDefinition: {
+                            name: 'a',
+                            content: [
+                                { validateInputAndGetOutput: { function: 'a', DB: 'a_DB', counter: 'a_counter', toVariable: 'output' } },
+                                { increaseCounterByOne: { counter: 'a_counter' } },
+                                { returnOutput: { returnedVariable: 'output' } }]
+                        }
+                    }
+                ]
+            }
+        ]
+        expect(spyJSONToHarnessJSON(spyJSON)).to.deep.equal(expectedJSON)
+    })
+})
